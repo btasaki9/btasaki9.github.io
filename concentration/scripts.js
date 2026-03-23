ce5c9798e08d0bf9d478d2b5839daf98a2803268
btasaki9 <benichiro.tasaki@wsu.edu>
2026-03-23 01:20:35 +0000

    watched half of the explainer video but code doesnt work

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -6,27 +6,33 @@
 function flipCard() {
 
     //get all the clicked cards 
-    let allClickedCards = document.quearySelectorAll(".clicked");
+    let allClickedCards = document.querySelectorAll(".clicked");
 
     //only proceeds if there are less than 2 clicked 
     if (allClickedCards.length < 2) {
-        //add clicked clas to clicked card 
-    this.classList.add("clicked");
-
+        //add clicked class to clicked card 
+        this.classList.add("clicked");
     }
 
     //get a fresh list of clicked cards
-    let allClickedCards = document.quearySelectorAll(".clicked");
+    allClickedCards = document.querySelectorAll(".clicked");
 
     //if it's a pair compare them 
     if (allClickedCards.length == 2) {
 
+        //get the class list of each card as a string 
         let card1 = allClickedCards[0].classList.toString();
         let card2 = allClickedCards[1].classList.toString();
 
+        //if the class lists match it's a pair
         if (card1 == card2) {
             console.log("It's a match!");
-        }else {
+            allClickedCards[0].classList.add("matched");
+            allClickedCards[1].classList.add("matched");
+            allClickedCards[0].classList.remove("clicked");
+            allClickedCards[1].classList.remove("clicked");
+        }
+        else {
             console.log("Not a match!");
         }
 
@@ -47,10 +53,13 @@ document.addEventListener("DOMContentLoaded", function(e) {
     let gameBoard = document.querySelector("#gameboard");
 
     //randomize cards by looping through the cards 
-    for (let x = 0; x < allCards.length; x++) {    
-        let randnum = Math.floor(Math.random() * allCards.length);
-        gameboard.insertBefore( allCards[x], gameboard.children [randnum]);
+    for (let x = 0; x < allCards.length; x++) { 
+           
+        let randNum = Math.floor(Math.random() * allCards.length);
+        gameBoard.insertBefore(allCards[x], gameBoard.children[randNum]);
+     
+    allCards[x].addEventListener("click", flipCard);
+
     }
 
-    allCards[x].addEventListener("click", flipCard);
    });
