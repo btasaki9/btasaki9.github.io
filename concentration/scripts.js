c40c56a4d38075a23fa3e15ceef2cf64676be2f2
btasaki9 <benichiro.tasaki@wsu.edu>
2026-03-23 00:11:31 +0000

    added added a loop for the cards in concentration

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,1 +1,29 @@
 //javascript for concentration game for 1 player
+
+
+
+//new function to flip card when clicked 
+function flipCard() {
+    this.classlist.add("clicked");
+}
+
+
+
+
+
+
+//runs the code when the dom is loaded
+document.addEventListener("DOMContentLoaded", function(e) {
+
+    //get handles to game elements 
+    let allCards = document.querySelectorAll(".card");
+    let gameBoard = document.querySelector("#gameboard");
+
+    //randomize cards by looping through the cards 
+    for (let x = 0; x < allCards.length; x++) {    
+        let randnum = Math.floor(Math.random() * allCards.length);
+        gameboard.insertBefore( allCards[x], gameboard.children [randnum]);
+    }
+
+    allCards[x].addEventListener("click", flipCard);
+   });
