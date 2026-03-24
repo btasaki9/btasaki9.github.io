4d0093cacabcf46738de6f1b4bf07f05caeb52e6
btasaki9 <benichiro.tasaki@wsu.edu>
2026-03-24 02:01:31 +0000

    added mrore jeva script from the video to increase player count and keep track of turns

--- concentration/scripts.js
+++ concentration/scripts.js
@@ -1,43 +1,61 @@
 //javascript for concentration game for 1 player
 
+//track number of turns
+let playerClicks = 0;
 
+//clear clicked classes
+function clearClicks() {
+    let allClickedCards = document.querySelectorAll(".clicked");
+    for (let eachCard of allClickedCards) {
+        eachCard.classList.remove("clicked");
+    }
+
+    //increase player turn count by 1 
+    playerClicks++;
+    document.querySelector("#turnCount span").innerHTML = playerClicks;   
+}
 
 //new function to flip card when clicked 
 function flipCard() {
 
-    //get all the clicked cards 
-    let allClickedCards = document.querySelectorAll(".clicked");
+    if (!this.classList.contains("matched")) {
 
-    //only proceeds if there are less than 2 clicked 
-    if (allClickedCards.length < 2) {
-        //add clicked class to clicked card 
-        this.classList.add("clicked");
-    }
 
-    //get a fresh list of clicked cards
-    allClickedCards = document.querySelectorAll(".clicked");
 
-    //if it's a pair compare them 
-    if (allClickedCards.length == 2) {
+        //get all the clicked cards 
+        let allClickedCards = document.querySelectorAll(".clicked");
 
-        //get the class list of each card as a string 
-        let card1 = allClickedCards[0].classList.toString();
-        let card2 = allClickedCards[1].classList.toString();
-
-        //if the class lists match it's a pair
-        if (card1 == card2) {
-            console.log("It's a match!");
-            allClickedCards[0].classList.add("matched");
-            allClickedCards[1].classList.add("matched");
-            allClickedCards[0].classList.remove("clicked");
-            allClickedCards[1].classList.remove("clicked");
-        }
-        else {
-            console.log("Not a match!");
+        //only proceeds if there are less than 2 clicked 
+        if (allClickedCards.length < 2) {
+            //add clicked class to clicked card 
+            this.classList.add("clicked");
         }
 
+        //get a fresh list of clicked cards
+        allClickedCards = document.querySelectorAll(".clicked");
+
+        //if it's a pair compare them 
+        if (allClickedCards.length == 2) {
+
+            //get the class list of each card as a string 
+            let card1 = allClickedCards[0].classList.toString();
+            let card2 = allClickedCards[1].classList.toString();
+
+            //if the class lists match it's a pair
+            if (card1 == card2) {
+                console.log("It's a match!");
+                allClickedCards[0].classList.add("matched");
+                allClickedCards[1].classList.add("matched");
+                window.setTimeout(clearClicks, 2000);
+            }
+            else {
+                console.log("Not a match!");
+                window.setTimeout(clearClicks, 2000);
+            }
+
+        }
     }
-    
+
 }
 
 
@@ -46,20 +64,20 @@ function flipCard() {
 
 
 //runs the code when the dom is loaded
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener("DOMContentLoaded", function (e) {
 
     //get handles to game elements 
     let allCards = document.querySelectorAll(".card");
     let gameBoard = document.querySelector("#gameboard");
 
     //randomize cards by looping through the cards 
-    for (let x = 0; x < allCards.length; x++) { 
-           
+    for (let x = 0; x < allCards.length; x++) {
+
         let randNum = Math.floor(Math.random() * allCards.length);
         gameBoard.insertBefore(allCards[x], gameBoard.children[randNum]);
-     
-    allCards[x].addEventListener("click", flipCard);
+
+        allCards[x].addEventListener("click", flipCard);
 
     }
 
-   });
+});
