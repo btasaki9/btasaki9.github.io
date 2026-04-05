1381e8253d745a5d9eb817f20f2e5946ae65a1f0
btasaki9 <benichiro.tasaki@wsu.edu>
2026-04-05 19:46:12 +0000

    added comments to the java script for wether app

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -1,31 +1,52 @@
 /* javascript to enabled drag-scrolling */
 
+//preparing variables
 let scrollingBox;
 let offsetLeftStart;
 let scrollLeftStart;
 let isMoving;
 
+
+//function to get remove JSON data
+async function getData(url, options) {
+    try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+            const result = await response.json();
+            return result;
+        } else {
+            throw (response.status);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//wait for the DOM to load before running the code
 document.addEventListener("DOMContentLoaded", function () {
-  scrollingBox = document.querySelector("#futureinfo"); /* get a handle on the parent container by tag or ID */
-  isMoving = false;
-
-  scrollingBox.addEventListener("mousedown", function (e) {
-    scrollLeftStart = scrollingBox.scrollLeft;
-    offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
-    isMoving = true;
-  });
-
-  scrollingBox.addEventListener("mouseleave", function (e) {
+    scrollingBox = document.querySelector("#futureinfo"); /* get a handle on the parent container by tag or ID */
     isMoving = false;
-  });
-  
-  scrollingBox.addEventListener("mouseup", function (e) {
-    isMoving = false;
-  });
 
-  scrollingBox.addEventListener("mousemove", function (e) {
-    e.preventDefault();
-    if (!isMoving) return;
-    scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
-  });
+    scrollingBox.addEventListener("mousedown", function (e) {
+        scrollLeftStart = scrollingBox.scrollLeft;
+        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
+        isMoving = true;
+    });
+
+    scrollingBox.addEventListener("mouseleave", function (e) {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mouseup", function (e) {
+        isMoving = false;
+    });
+
+    scrollingBox.addEventListener("mousemove", function (e) {
+        e.preventDefault();
+        if (!isMoving) return;
+        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
+    });
+
+
+    
 });
