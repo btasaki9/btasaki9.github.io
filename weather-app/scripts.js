abee664d3bf273e888d45bf84aa0acb1a85ab12e
btasaki9 <benichiro.tasaki@wsu.edu>
2026-04-05 23:33:55 +0000

    progressed in the video but still not working

--- weather-app/scripts.js
+++ weather-app/scripts.js
@@ -24,58 +24,61 @@ async function getData(url, options) {
 
 //update weather display in the DOM based on passed object 
 function updateWeather(weatherObject) {
-   console.log(weatherObject);
-   document.querySelector("#currenttemp span").innerHTML = weatherObject.current.temp_f;
-   document.querySelector("#currentstatus").innerHTML = weatherObject.current.condition.text;
-   document.querySelector("#currenthumidity span").innerHTML = weatherObject.current.humidity;
+    console.log(weatherObject);
+    document.querySelector("#currenttemp span").innerHTML = weatherObject.current.temp_f;
+    document.querySelector("#currentstatus").innerHTML = weatherObject.current.condition.text;
+    document.querySelector("#currenthumidity span").innerHTML = weatherObject.current.humidity;
 
-   let windspeed = weatherObject.current.wind_mph;
-   let winddirection = weatherObject.current.wind_dir;
-   document.querySelector("#currentwind").innerHTML = windspeed + "mph " + winddirection;
+    let windspeed = weatherObject.current.wind_mph;
+    let winddirection = weatherObject.current.wind_dir;
+    document.querySelector("#currentwind").innerHTML = windspeed + "mph " + winddirection;
 
-   let futuredays = document.querySelectorAll(".futureday");
-}
+    let futuredays = document.querySelectorAll(".futureday");
+    for (i = 0; i < futuredays.length; i++) {
+        futuredays[i].querySelector(".futuretemp").innerHTML = weatherObject.forecast.forecastday[i].day.maxtemp_f;
+    
+    }
 
 
 
-//wait for the DOM to load before running the code
-document.addEventListener("DOMContentLoaded", function () {
-    scrollingBox = document.querySelector("#futureinfo"); /* get a handle on the parent container by tag or ID */
-    isMoving = false; 
-
-    scrollingBox.addEventListener("mousedown", function (e) {
-        scrollLeftStart = scrollingBox.scrollLeft;
-        offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
-        isMoving = true;
-    });
-
-    scrollingBox.addEventListener("mouseleave", function (e) {
+    //wait for the DOM to load before running the code
+    document.addEventListener("DOMContentLoaded", function () {
+        scrollingBox = document.querySelector("#futureinfo"); /* get a handle on the parent container by tag or ID */
         isMoving = false;
+
+        scrollingBox.addEventListener("mousedown", function (e) {
+            scrollLeftStart = scrollingBox.scrollLeft;
+            offsetLeftStart = e.pageX - scrollingBox.offsetLeft;
+            isMoving = true;
+        });
+
+        scrollingBox.addEventListener("mouseleave", function (e) {
+            isMoving = false;
+        });
+
+        scrollingBox.addEventListener("mouseup", function (e) {
+            isMoving = false;
+        });
+
+        scrollingBox.addEventListener("mousemove", function (e) {
+            e.preventDefault();
+            if (!isMoving) return;
+            scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
+        });
+
+
+        let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js";
+        let sampleOptions = {};
+
+
+        //get sample data 
+        getData(sampleURL, sampleOptions).then(function (result) {
+            // code to operate on “result” JSON object
+            updateWeather(result);
+        });
+
+
+
+
+
     });
-
-    scrollingBox.addEventListener("mouseup", function (e) {
-        isMoving = false;
-    });
-
-    scrollingBox.addEventListener("mousemove", function (e) {
-        e.preventDefault();
-        if (!isMoving) return;
-        scrollingBox.scrollLeft = scrollLeftStart - (e.pageX - offsetLeftStart - scrollingBox.offsetLeft);
-    });
-
-
-    let sampleURL = "https://tordevries.github.io/477/examples/ajax-api-test/current-forecast.js";
-    let sampleOptions = {};
-
-
-    //get sample data 
-    getData(sampleURL, sampleOptions).then(function (result) {
-        // code to operate on “result” JSON object
-        updateWeather(result);
-    });
-
-
-
-
-
-});
